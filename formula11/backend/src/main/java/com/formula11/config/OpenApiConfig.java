f91392e1a53c957ba2ef055b5fba48375d3d8b07
Lucrecia <lucrecolon1828@gmail.com>
2026-09-18 20:07:22 -0300

    personalizacion del swagger

---
+++ formula11/backend/src/main/java/com/formula11/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.formula11.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String BEARER_SCHEME = "bearerAuth";
+
+    @Bean
+    public OpenAPI formula11OpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Formula11 API")
+                        .description("API de valoración de mercado de jugadores de fútbol")
+                        .version("v1.40")
+                        .contact(new Contact()
+                                .name("Formula11 Team")
+                                .email("contacto@formula11.com")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_SCHEME, new SecurityScheme()
+                                .name(BEARER_SCHEME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
